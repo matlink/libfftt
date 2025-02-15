130345eba2d28dd1a2be86ec9dca62ded7cfd977
Matlink <matlink@matlink.fr>
2025-02-15 17:57:10 +0100

    chore: format

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
+mod club;
 mod joueur;
+const API: &str = "http://fftt.dafunker.com/v1";
 
 #[cfg(test)]
 mod tests {
     use crate::joueur::Joueur;
 
-    use super::*;
-
     #[tokio::test]
     async fn test_joueur() {
-        let joueur = Joueur::new("3421810").await.expect("Erreur lors de la récupération du joueur");
+        let joueur = Joueur::new("3421810")
+            .await
+            .expect("Erreur lors de la récupération du joueur");
         assert_eq!(joueur.nom, "LEBRUN");
         assert_eq!(joueur.prenom, "Felix");
         assert_eq!(joueur._licence, "3421810");
